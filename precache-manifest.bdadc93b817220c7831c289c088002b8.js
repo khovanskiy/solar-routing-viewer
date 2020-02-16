acb4d3a92d5103fc6e5ff379441b9f0216816a94
Victor Khovanskiy <victor.khovanskiy@gmail.com>
2020-02-16 15:28:59 +0300

    Updates

---
+++ precache-manifest.bdadc93b817220c7831c289c088002b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394290e7a036264adfea709407fcc36f",
+    "url": "/solar-routing-viewer/index.html"
+  },
+  {
+    "revision": "12165b1db60e900be283",
+    "url": "/solar-routing-viewer/static/css/main.ef5f6db0.chunk.css"
+  },
+  {
+    "revision": "476ef26bce5f446bad5b",
+    "url": "/solar-routing-viewer/static/js/2.33c6c62b.chunk.js"
+  },
+  {
+    "revision": "8fe9c07f83e99a7ad0c4eaa31741a36a",
+    "url": "/solar-routing-viewer/static/js/2.33c6c62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12165b1db60e900be283",
+    "url": "/solar-routing-viewer/static/js/main.94a99078.chunk.js"
+  },
+  {
+    "revision": "0302e6b1a816a6364d5e",
+    "url": "/solar-routing-viewer/static/js/runtime-main.7eb0d98b.js"
+  }
+]);
