fcefe373749c11b5b7857e6173028c9695cffe2c
Victor Khovanskiy <victor.khovanskiy@gmail.com>
2020-02-21 01:56:17 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/solar-routing-viewer/precache-manifest.19806d069def7f25d26ac0ec1eede6c7.js"
+  "/solar-routing-viewer/precache-manifest.a31e96b213e8acbd3211c53cf293e613.js"
 );
 
 self.addEventListener('message', (event) => {
